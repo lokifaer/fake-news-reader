fa897a0a836f2b902c852a6fc50682b993b46298
rm-gh <raphael.mollon@gmail.com>
2016-08-30 15:15:51 +0200

    Initial commit

---
+++ app/src/main/java/com/oc/rss/fakenewsreader/MainActivity.java
@@ -0,0 +1,24 @@
+package com.oc.rss.fakenewsreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.oc.rss.fake.FakeNews;
+import com.oc.rss.fake.FakeNewsList;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final RecyclerView rv = (RecyclerView) findViewById(R.id.fakelist);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(new MyAdapter);
+    }
+}
---
+++ app/src/main/java/com/oc/rss/fakenewsreader/MyAdapter.java
@@ -0,0 +1,44 @@
+package com.oc.rss.fakenewsreader;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.oc.rss.fake.FakeNews;
+import com.oc.rss.fake.FakeNewsList;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by rmollon on 30/08/16.
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    List<FakeNews> list = FakeNewsList.all;
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.activity_main, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        String newsTitle = list.get(position).title;
+        holder.display(newsTitle);
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private final TextView title;
+    }
+}
